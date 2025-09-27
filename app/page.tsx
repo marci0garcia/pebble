280a04a2574b67b8a512bebf67754ad1b0043694
carlos-manuel-diaz <43973506+carlos-manuel-diaz@users.noreply.github.com>
2025-09-27 14:02:15 -0400

    Updated Frontend of Landing Page

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
+import { InboxIcon } from '@heroicons/react/24/outline';
+
 
 
 
@@ -10,23 +12,23 @@ export default function Page() {
   return (
 
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-red-600 p-4 md:h-35">
         {<AcmeLogo />}
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-        <div className="relative w-0 h-0 border-l-[15px] border-r-[15px] border-b-[26px] border-l-transparent border-r-transparent border-b-black"/>
+              <InboxIcon className="h-12 w-12 " />
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Pebble.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
+            <strong>Welcome to Pebble!</strong> This is a open source {' '}
+            <a href="https://github.com/marci0garcia/pebble" className="text-red-600">
+              open source
             </a>
-            , brought to you by Vercel.
+            {' '} task management system for for teams.
             
           </p>
           <Link
             href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+            className="flex items-center gap-5 self-start rounded-lg bg-red-600 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-red-400 md:text-base"
           >
             <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
           </Link>
@@ -34,7 +36,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-            src="/hero-desktop.png"
+            src="/RocteraSystems.jpg"
             width={1000}
             height={760}
             className="hidden md:block"
