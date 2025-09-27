58b3e1c681d7719ba5af9c6030d383978cf8c902
carlos-manuel-diaz <43973506+carlos-manuel-diaz@users.noreply.github.com>
2025-09-27 14:38:17 -0400

    Font Changes

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,11 +1,13 @@
 import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
-import { lusitana } from '@/app/ui/fonts';
+import { audiowide, lusitana } from '@/app/ui/fonts';
 import { fetchCardData} from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton} from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Audiowide } from 'next/font/google';
+
 
 export default async function Page() {
   
@@ -17,7 +19,7 @@ export default async function Page() {
   } = await fetchCardData();
   return (
     <main>
-      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h1 className={`${audiowide.className} mb-4 text-xl md:text-2xl`}>
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
